aa9b22184d78cb26f0c320135ab14d93e618cd19
Kuro <45374906+kuro250104@users.noreply.github.com>
2024-02-16 00:18:35 +0100

    first commit

---
+++ migrations/Version20240118155732.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240118155732 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipes ADD name VARCHAR(255) NOT NULL, ADD images VARCHAR(255) NOT NULL, ADD ingredient VARCHAR(255) NOT NULL, ADD utensil VARCHAR(255) NOT NULL, ADD instruction VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipes DROP name, DROP images, DROP ingredient, DROP utensil, DROP instruction');
+    }
+}
